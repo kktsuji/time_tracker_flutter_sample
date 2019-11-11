cf94ddd6d34150b8cccf1352cb75aee3ccd6bb75
Kouki Tsuji <cookieee.626+tech@gmail.com>
2019-11-11 20:14:13 +0900

    Add logout

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -8,10 +9,20 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-
   FirebaseUser _user;
 
-  void _updateUser(FirebaseUser user){
+  @override
+  void initState() {
+    super.initState();
+    _checkCurrentUser();
+  }
+
+  Future<void> _checkCurrentUser() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    _updateUser(user);
+  }
+
+  void _updateUser(FirebaseUser user) {
     //print('User id: ${user.uid}');
     setState(() {
       _user = user;
@@ -22,9 +33,11 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     if (_user == null) {
       return SignInPage(
-        onSignIn: _updateUser,  // コンストラクタに関数 _updateUser を渡している
+        onSignIn: _updateUser, // コンストラクタに関数 _updateUser を渡している
       );
     }
-    return Container();
+    return HomePage(
+      onSignOut: () => _updateUser(null), // 同じく渡している
+    );
   }
 }
